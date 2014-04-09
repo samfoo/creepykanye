d37aa3976b0e03795ff858231cde48e8f09010bb
Sam Gibson <sam@ifdown.net>
2014-04-09 14:57:01 +1000

    Display stuff from the webcam

--- project.clj
+++ project.clj
@@ -5,7 +5,16 @@
             :url "https://www.gnu.org/copyleft/gpl.html"}
   :plugins [[lein-localrepo "0.5.2"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.jodd/jodd-swingspy "3.4.5"]
+                 [seesaw "1.4.4"]
+
+                 ;; External/native dependencies. See babushka-deps/javacv.rb
+                 ;; for installation instructions
                  [opencv-native "2.4.8"]
                  [javacv "0.7"]
                  [javacpp "0.7"]
-                 [javacv-macosx-x86_64 "0.7"]])
+                 [ffmpeg "2.1.1"]
+                 [javacv-macosx-x86_64 "0.7"]]
+  :profiles {:gui {:main creepykanye.core
+                   :aot :all
+                   :uberjar-name "gui.jar"}})
--- src/creepykanye/core.clj
+++ src/creepykanye/core.clj
@@ -1,6 +1,45 @@
-(ns creepykanye.core)
+(ns creepykanye.core
+  (:require [creepykanye.recognize]
+            [seesaw.graphics :as graphics]
+            [seesaw.core :refer :all]
+            [seesaw.bind :as b])
+  (:gen-class :main true)
+  (:import [com.googlecode.javacv
+            CanvasFrame
+            FrameGrabber
+            OpenCVFrameGrabber]
+           [com.googlecode.javacv.cpp opencv_highgui]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- grab-image [grabber]
+  (.grab grabber))
+
+
+(defn- display-image [canvas image]
+  (.showImage canvas image))
+
+(defn paint-image [c g image]
+  (.drawImage g image 0 0 (.getWidth image) (.getHeight image) nil))
+
+(defn show-images [grabber image screen]
+  (loop []
+    (let [cap (grab-image grabber)]
+      (reset! image (.getBufferedImage cap))
+      (repaint! screen))
+    (recur)))
+
+(defn -main []
+  (let [image (atom nil)
+        grabber (OpenCVFrameGrabber. 0)]
+    (.start grabber)
+    (let [width (.getImageWidth grabber)
+          height (.getImageHeight grabber)
+          screen (canvas :id :screen
+                         :paint (fn [c g] (paint-image c g @image))
+                         :background :black)
+          window (frame :title "Camera"
+                        :width width
+                        :height height
+                        :visible? true
+                        :on-close :dispose
+                        :content screen)]
+      (show-images grabber image screen))))
