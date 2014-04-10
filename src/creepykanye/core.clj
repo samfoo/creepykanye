960ab8b8e49b5876477cbcff4ae3d725ba069595
Sam Gibson <sam@ifdown.net>
2014-04-10 12:02:30 +1000

    Start capturing faces

--- src/creepykanye/core.clj
+++ src/creepykanye/core.clj
@@ -6,7 +6,8 @@
             [seesaw.core :refer :all]
             [seesaw.bind :as b])
   (:gen-class :main true)
-  (:import [com.googlecode.javacv
+  (:import [javax.imageio.ImageIO]
+           [com.googlecode.javacv
             CanvasFrame
             FrameGrabber
             OpenCVFrameGrabber]
@@ -15,17 +16,30 @@
 (defn- grab-image [grabber]
   (.grab grabber))
 
-
 (defn- display-image [canvas image]
   (.showImage canvas image))
 
 (defn paint-image [c g image face]
-  (.drawImage g image 0 0 (.getWidth image) (.getHeight image) nil)
-  (when (not (nil? face))
-    (graphics/draw g
-                   (graphics/rect (:x face) (:y face)
-                                  (:width face) (:height face))
-                   (graphics/style :foreground :green :stroke 5))))
+  (when (not (nil? image))
+    (.drawImage g image 0 0 (.getWidth image) (.getHeight image) nil)
+    (when (not (nil? face))
+      (graphics/draw g
+                     (graphics/rect (:x face) (:y face)
+                                    (:width face) (:height face))
+                     (graphics/style :foreground :green :stroke 5)))))
+
+(defn build-corpus [image face]
+  (loop []
+    (when (not (nil? @face))
+      (let [file (clojure.java.io/file
+                  (str
+                   "corpus/" (System/currentTimeMillis) ".png"))]
+        (println "saving to " (.getAbsolutePath file))
+        (javax.imageio.ImageIO/write @image "png" file)
+        (println "sleeping")
+        (Thread/sleep 500)
+        (println "done sleeping")))
+     (recur)))
 
 (defn show-images [grabber image face screen]
   (let [detector (faces/detector)]
@@ -53,4 +67,7 @@
                         :visible? true
                         :on-close :dispose
                         :content screen)]
-      (show-images grabber image face screen))))
+      (future
+        (build-corpus image face))
+      (future
+        (show-images grabber image face screen)))))
