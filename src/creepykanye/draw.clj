23e3b7a6e5eaa82ed9918f7cfcd93ef03afd0437
Sam Gibson <sam@ifdown.net>
2014-04-13 14:35:52 +1000

    Color different faces differently

--- src/creepykanye/draw.clj
+++ src/creepykanye/draw.clj
@@ -3,21 +3,41 @@
             [seesaw.color :as color])
   (:import [java.awt Font]))
 
+(def colors [:tomato
+              :aqua
+              :cyan
+              :hotpink
+              :lime
+              :peru
+              :yello
+              :plum])
+
+(defn- choose-color [i]
+  (get colors (mod i (count colors))))
+
 (defn outline [g objects]
-  (doseq [object objects]
-    (graphics/draw g
-                   (graphics/rect (:x object) (:y object)
-                                  (:width object) (:height object))
-                   (graphics/style :foreground :green :stroke 5))))
+  (loop [os objects
+         i 0]
+    (when (not (empty? os))
+      (let [object (first os)]
+        (graphics/draw g
+                       (graphics/rect (:x object) (:y object)
+                                      (:width object) (:height object))
+                       (graphics/style :foreground (choose-color i) :stroke 5))
+        (recur (rest os) (inc i))))))
 
 (defn label [g objects]
-  (doseq [object objects]
-    (let [label (:label object)]
-      (when (not (nil? label))
-        (do
-          (.setColor g (color/to-color "#FF0000"))
-          (.setFont g (Font. "Helvetica" Font/BOLD 24))
-          (.drawString g
-                       (str label)
-                       (:x object)
-                       (+ (:height object) (:y object))))))))
+  (loop [os objects
+         i 0]
+    (when (not (empty? os))
+      (let [object (first os)
+            label (:label object)]
+        (when (not (nil? label))
+          (do
+            (.setColor g (color/to-color (choose-color i)))
+            (.setFont g (Font. "Helvetica" Font/BOLD 24))
+            (.drawString g
+                         (str label)
+                         (:x object)
+                         (+ (:height object) (:y object) 24)))))
+      (recur (rest os) (inc i)))))
